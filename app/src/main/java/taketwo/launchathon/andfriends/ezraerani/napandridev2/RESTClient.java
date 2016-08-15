42de8dd3d7e6cc8d38ca8c875c5ed5953a6e21e8
emerani <ezraerani@gmail.com>
2016-08-15 14:12:08 -0400

    update RESTClient

--- app/src/main/java/taketwo/launchathon/andfriends/ezraerani/napandridev2/RESTClient.java
+++ app/src/main/java/taketwo/launchathon/andfriends/ezraerani/napandridev2/RESTClient.java
@@ -1,8 +1,31 @@
 package taketwo.launchathon.andfriends.ezraerani.napandridev2;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by ezraerani on 8/12/16.
  */
 public class RESTClient {
+
+    public APIClient apiService;
+
+    RESTClient() {
+        Gson gson = new GsonBuilder().create();
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://maps.googleapis.com/maps/api/directions/")
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+
+        apiService = retrofit.create(APIClient.class);
+
+    }
+
+    public APIClient getApiClient() {
+        return apiService;
+    }
 
 }
